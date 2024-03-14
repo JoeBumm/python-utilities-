6d53c46ab45b7b0a7370dc2d5874b253e1c2df63
JoeBum <66751176+JoeBumm@users.noreply.github.com>
2024-03-14 18:56:57 +0200

    The sup function is defined before it is called. The if-elif-else structure inside the sup function is fixed to correctly check the value of x. The flag variable is properly handled within the sup function and checked at the end to determine the output message. Indentation and syntax errors are corrected to ensure the code runs smoothly.

--- shutdown.py
+++ shutdown.py
@@ -1,41 +1,40 @@
 import os
-import math
-def lines(i):
-    i = 0
-    while i<5: 
-        print("....................")
-        i+=1
 
-print("This tool will make you schedule shutdowns!")
-print("Please Choose when do you want to schedule (Write in minutes) ")
+def lines(i):
+    for _ in range(i):
+        print("....................")
+
+print("This tool will help you schedule shutdowns!")
+print("Please choose when you want to schedule (Write in minutes)")
 lines(1)
 
-print("type 1 for instant shutdown")
-print("type 2 for 60 minutes")
-print("type 3 for 2 hours")
-print("type 4 for 3 hours")
+print("Type 1 for instant shutdown")
+print("Type 2 for 60 minutes")
+print("Type 3 for 2 hours")
+print("Type 4 for 3 hours")
 
 x = int(input("Enter: "))
 
-while (x<0 or x>6000):
+while x < 0 or x > 6000:
     print("Enter a suitable time")
     x = int(input("Enter: "))
 
-def scheduler
-if x==1:
-    os.system("shutdown /t /s 1")
-if x==2:
-    os.system("shutdown /t /s 3600")
-if x==3:
-    os.system("shutdown /t /s 7200")
-if x==4:
-    os.system("shutdown /t /s 14400")
+def sup(x):
+    if x == 1:
+        os.system("shutdown /s /t 1")
+    elif x == 2:
+        os.system("shutdown /s /t 3600")
+    elif x == 3:
+        os.system("shutdown /s /t 7200")
+    elif x == 4:
+        os.system("shutdown /s /t 14400")
+    else:
+        print("This option does not exist")
 
+flag = False
+sup(x)
 
-
-
-
-
-
-    
-
+if not flag:
+    print("Your shutdown is scheduled.")
+else:
+    print("Please rerun the program.")
