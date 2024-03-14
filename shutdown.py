2adb065cf7c0a7fd9bf45ab48cbd3fde7c7858f9
JoeBum <66751176+JoeBumm@users.noreply.github.com>
2024-03-14 18:19:10 +0200

    Access to System

---
+++ shutdown.py
@@ -0,0 +1,18 @@
+import os
+
+def lines(i):
+    i = 0
+    while i<5: 
+        print("....................")
+        i+=1
+
+print("Hello, This tool will help you install a wsl machine on your computer !")
+lines(1)
+
+
+
+
+
+
+    
+
