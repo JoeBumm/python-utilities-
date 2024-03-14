6e9ce6a34930393d608841692ff41d0d4989edf9
JoeBum <66751176+JoeBumm@users.noreply.github.com>
2024-03-14 18:42:36 +0200

    User Friendly promts

--- shutdown.py
+++ shutdown.py
@@ -1,13 +1,21 @@
 import os
-
+import math
 def lines(i):
     i = 0
     while i<5: 
         print("....................")
         i+=1
 
-print("Hello, This tool will help you install a wsl machine on your computer !")
+print("This tool will make you schedule shutdowns!")
+print("Please Choose when do you want to schedule (Write in minutes) ")
 lines(1)
+x = int(input("Enter: "))
+
+while (x<0 or x>6000):
+    print("Enter a suitable time")
+    x = int(input("Enter: "))
+
+
 
 
 
